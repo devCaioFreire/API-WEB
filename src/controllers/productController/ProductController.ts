870583e52a9180907efeca122f167c3c5ab42ff5
Caio Freire <caiohenriquefreire@icloud.com>
2023-09-05 15:42:41 -0300

    .

--- src/controllers/productController/ProductController.ts
+++ src/controllers/productController/ProductController.ts
@@ -1,37 +1,37 @@
 import { Request, Response } from "express";
-import { ParamProps, ProductService, ParamFilter } from '../../services/productsService/ProductService';
+import { ParamFilter, ParamProps, ProductService } from '../../services/productsService/ProductService';
 
 
 export class ProductController {
   async handle(req: Request, res: Response) {
     const requisition = req;
     const params = req.query;
-    let ParamConfig: ParamProps[]=[];
-    const ParamFilter: ParamFilter[]=[];
+    let ParamConfig: ParamProps[] = [];
+    const ParamFilter: ParamFilter[] = [];
 
     Object.entries(params).map(([field, value]) => (
-      ParamConfig.push({field:field,value:value})
-      )); 
+      ParamConfig.push({ field: field, value: value })
+    ));
 
     const filters = ParamConfig.filter(element => element.field === 'filter');
 
     ParamConfig = ParamConfig.filter(element => element.field != 'filter');
-    if(filters != undefined && filters != null){
-      for( const filter of filters){
+    if (filters != undefined && filters != null) {
+      for (const filter of filters) {
         const json = JSON.parse(filter.value)
-        json.forEach(element => {
-          ParamFilter.push({field:element.field,value:element.value})
+        json.forEach((element: { field: any; value: any; }) => {
+          ParamFilter.push({ field: element.field, value: element.value })
         });
       }
-    
-    // // Recupere o código EAN dos parâmetros da URL
+
+      // Recupere o código EAN dos parâmetros da URL
 
       const productService = new ProductService();
-      const product = await productService.get(productService.ParamPropsFormater(ParamFilter),ParamConfig);
+      const product = await productService.get(productService.ParamPropsFormater(ParamFilter), ParamConfig);
       if (!product) {
         return res.status(404).json({ error: 'Produto não encontrado' });
       }
       return res.json(product);
+    }
   }
-}
 }
--- src/services/productsService/ProductService.ts
+++ src/services/productsService/ProductService.ts
@@ -1,5 +1,4 @@
 import { prismaMain } from "../../prisma";
-import { produtos } from '../../../prisma/databases/main/index';
 
 export interface ParamProps {
   field: string;
@@ -9,11 +8,11 @@ export interface ParamFilter {
   field: string;
   value: any;
 }
-export interface IQuery{
-  where?:Record<string, string | number | boolean>;
-  take?:number;
-  skip?:number;
-  orderBy?:any;
+export interface IQuery {
+  where?: Record<string, string | number | boolean>;
+  take?: number;
+  skip?: number;
+  orderBy?: any;
 }
 export class ProductService {
   async execute(ean: string) {
@@ -26,45 +25,45 @@ export class ProductService {
       throw new Error('Erro ao buscar produtos no banco de dados');
     }
   }
-  async get(selectors?: ParamFilter[], params?:ParamProps[]){
+  async get(selectors?: ParamFilter[], params?: ParamProps[]) {
     const query: IQuery = {};
-     //where
-		if (selectors && selectors.length > 0) {
-			query.where = {};
-			for (const filter of selectors) {
-				query.where[filter.field] = filter.value;
-			}
-		}
+    //where
+    if (selectors && selectors.length > 0) {
+      query.where = {};
+      for (const filter of selectors) {
+        query.where[filter.field] = filter.value;
+      }
+    }
     query.take = 20
     //propriedades
     if (params && params.length > 0) {
-			for (const param of params) {
-				switch (param.field) {
+      for (const param of params) {
+        switch (param.field) {
           case 'page':
-              query.skip = (param.value - 1) * query.take;
+            query.skip = (param.value - 1) * query.take;
             break;
           case 'orderBy':
-              query.orderBy = param.value;
+            query.orderBy = param.value;
             break;
         }
-			}
-		}
-		const produtos = await prismaMain.produtos.findMany({where:query.where, skip:query.skip,take:query.take, orderBy:query.orderBy});
-		await prismaMain.$disconnect();
-		return produtos;
+      }
+    }
+    const produtos = await prismaMain.produtos.findMany({ where: query.where, skip: query.skip, take: query.take, orderBy: query.orderBy });
+    await prismaMain.$disconnect();
+    return produtos;
   }
-  ParamPropsFormater(Params: ParamFilter[]){
+  ParamPropsFormater(Params: ParamFilter[]) {
     for (const param of Params) {
       switch (param.field) {
         case 'saldo':
-          if(param.value == 0 || param.value == '0'){
-            param.value = {lte: param.value}
+          if (param.value == 0 || param.value == '0') {
+            param.value = { lte: param.value }
             break;
           }
-          param.value = {gte: param.value};
+          param.value = { gte: param.value };
           break;
         case 'descricao':
-          param.value = {contains: param.value};
+          param.value = { contains: param.value };
           break;
       }
     }
